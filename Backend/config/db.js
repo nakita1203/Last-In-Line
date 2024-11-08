3aea84cdb6e917d6e8693122d5739a1dcdc88936
arifa <arifatulalya417e@gmail.com>
2024-11-08 07:24:45 +0700

    api working

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://test:yhkRBlIRvwecL123@test.hwz0iah.mongodb.net/').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://arifatalya:5hgJjI6nS7qRzPL0@server.fybnr.mongodb.net/?retryWrites=true&w=majority&appName=Server').then(()=>console.log("DB Connected"))
 }
